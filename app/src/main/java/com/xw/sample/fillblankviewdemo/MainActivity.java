280e3b307e87a06fb1f45f6cc64b3b2f8042f84e
woxingxiao <Chauvet910@gmail.com>
2017-08-31 18:22:27 +0800

    Update demo 2.0

--- app/src/main/java/com/xw/sample/fillblankviewdemo/MainActivity.java
+++ app/src/main/java/com/xw/sample/fillblankviewdemo/MainActivity.java
@@ -5,37 +5,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.xw.repo.fillblankview.FillBlankView;
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String s1 = "password";
-    private static final String s2 = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FillBlankView passwordFillBlankView = (FillBlankView) findViewById(R.id.show_password_demo);
-        passwordFillBlankView.setBlankNum(s1.length());
-        passwordFillBlankView.setText(s1);
-        passwordFillBlankView.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.show_password_demo).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, PasswordDemoActivity.class));
             }
         });
 
-        FillBlankView textFillBlankView = (FillBlankView) findViewById(R.id.show_text_demo);
-        textFillBlankView.setBlankNum(s2.length());
-        textFillBlankView.setText(s2);
-        textFillBlankView.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.show_text_demo).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, TextDemoActivity.class));
             }
         });
     }
-
 }
